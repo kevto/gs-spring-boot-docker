dda261f40de56e83688c26e326e802dfb3de02ca
kevto <info@kevinberendsen.nl>
2017-09-22 11:16:28 +0200

    Added a new endpoint

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -14,6 +14,11 @@ public class Application {
         return "Hello Docker World";
     }
 
+    @RequestMapping("/new-endpoint")
+    public String newEndpoint() {
+	return "Amazing how this easily works.."
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
